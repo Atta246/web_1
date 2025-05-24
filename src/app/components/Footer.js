79f38b8116535e3229f69d779f6569e678db540d
Atta246 <166260871+Atta246@users.noreply.github.com>
2025-05-24 03:18:47 +0300

    Update Footer.js

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -46,7 +46,7 @@ export default function Footer() {  return (    <footer className="bg-gradient-t
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                   </svg>
                 </span>
-                (555) 123-4567
+                +201092289854
               </li>
               <li className="text-base text-gray-600 dark:text-gray-300 flex items-center rounded-xl px-4 py-3 transition-all duration-300 backdrop-blur-sm bg-white/40 dark:bg-gray-800/40 shadow-sm hover:shadow-md">
                 <span className="bg-gradient-to-br from-purple-400 to-purple-600 p-2 rounded-lg mr-3 shadow-md">
